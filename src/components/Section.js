8d78befa7962362a3d20e6419f6ca0b71663be9a
db360 <da.b@hotmail.es>
2022-02-03 15:07:55 +0100

    animations

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,32 +1,64 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import Modal from './Modal';
-import './styles/Section.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Modal from "./Modal";
+import "./styles/Section.css";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Section = () => {
   const [modalActive, setModalActive] = useState(false);
 
   const showModal = () => {
-    setModalActive(!modalActive)
-  }
+    setModalActive(!modalActive);
+  };
+
+  const titleVariants = {
+    hidden: {
+      x: -750,
+      opacity: 0,
+    },
+    show: { x: 0, opacity: 100 },
+    transition: {
+      staggerChildren: 1,
+      duration: 1,
+    },
+  };
+
+  const listItem = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 }
+  };
+
   return (
-        <div className='section'>
+    <div className="section">
+      <motion.div
+        variants={titleVariants}
+        transition={titleVariants.transition}
+        initial="hidden"
+        animate="show"
+      >
+        <motion.h1 variants={listItem} className="main-header">
+          My Name is <span>David Martínez</span>
+        </motion.h1>
+        <motion.p variants={listItem} className="main-status">
+          I am a Sound Tech who loves Music and Technology, i've been playing
+          with hardware since I was a kid, lately with 3D design and animation,
+          and now I've found that i can blend all my passions together trought
+          Web Development.
+        </motion.p>
+        <motion.div
+          variants={listItem}
+          className="main-btn"
+        >
+          <Link onClick={showModal} to="/">
+            See My Works
+          </Link>
+        </motion.div>
 
-          <div>
-            <h1 className='main-header'>My Name is <span>David Martínez</span></h1>
-            <p className='main-status'>I am a Sound Tech who loves Music and Technology, i've been playing with hardware since I was a kid, lately with 3D design and animation, and now I've found that i can blend all my passions together trought Web Development.</p>
-            <div className="main-btn">
-              <Link onClick={showModal} to="/">See My Works</Link>
-            </div>
-
-            {modalActive && <Modal showModal={showModal}/>}
-
-          </div>
-
-        </div>
-    );
+        {modalActive && <Modal showModal={showModal} />}
+      </motion.div>
+    </div>
+  );
 };
 
 export default Section;
